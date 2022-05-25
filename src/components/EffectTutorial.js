398bc1c847b15a531d73d83a409d7f21b1924c0e
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-05-25 16:55:44 -0500

    understanding useEffect more

--- src/components/EffectTutorial.js
+++ src/components/EffectTutorial.js
@@ -3,16 +3,28 @@ import axios from 'axios'
 
 function EffectTutorial() {
     const [data, setData] = useState("");
+    const [count, setCount] = useState(0);
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/comments")
+        axios
+        .get("https://jsonplaceholder.typicode.com/comments")
         .then((resp)=>{
             setData(resp.data[0].email)
+            console.log("Api was called")
         });
-    });
+    }, [count]);//useEffect called when count's state changes
     
   return (
-    <div>Hello World: {data}</div>
+    <div>
+        Hello World 
+        <h1>{data}</h1>
+        <h2>{count}</h2>
+        <button onClick={() => {
+            setCount(count + 1)
+        }}>
+            Click
+        </button>
+    </div>
   );
 };
 
