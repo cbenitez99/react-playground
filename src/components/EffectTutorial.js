161917c702e8da944567b9da2ba295921d4a750d
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-05-25 16:49:29 -0500

    useEffect in EFFECT

--- src/components/EffectTutorial.js
+++ src/components/EffectTutorial.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
 function EffectTutorial() {
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/comments")
+        .then((resp)=>{
+            setData(resp.data[0].email)
+        });
+    });
     
   return (
-    <div>Hello World</div>
-  )
+    <div>Hello World: {data}</div>
+  );
 };
 
 export default EffectTutorial;
