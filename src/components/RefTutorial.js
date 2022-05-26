4a8e1b89ad012ab21cf22c02a7236317a5716ea7
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-05-26 11:39:13 -0500

    capturing input with useRef()

--- src/components/RefTutorial.js
+++ src/components/RefTutorial.js
@@ -2,11 +2,17 @@ import React, {useRef} from 'react'
 
 function RefTutorial() {
 
+  const inputRef = useRef(null);
+
+  const onClick = () => {
+    console.log(inputRef.current.value)
+  };
+
   return (
     <div>
         <h1>Cristian</h1>
-        <input type="text" placeholder="Ex..."/>
-        <button>Change Name</button>
+        <input type="text" placeholder="Ex..." ref={inputRef}/>
+        <button onClick={onClick}>Change Name</button>
     </div>
   )
 };
