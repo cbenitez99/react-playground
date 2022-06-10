92426a4e759712c6bf3dc83644775716c1f49e04
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-06-10 15:39:12 -0500

    Memo Hook practice

---
+++ src/components/Memo/MemoTutorial.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react'
+
+
+function MemoTutorial() {
+
+    const [data, setData] = useState(null); 
+
+    useEffect(() => {
+      axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response)=> {
+          setData(response.data);
+      });
+    }, []);
+
+    const findLongestName = (comments) => {
+        if (!comments) return null;
+
+        let longestName = "";
+        for (let i = 0; i < comments.length; i++) {
+            let currentName = comments[i].name;
+            if (currentName.length > longestName.length) {
+                longestName = currentName
+            }
+        }
+    }
+
+    console.log("THIS WAS COMPUTED");
+    
+  return (
+    <div>MemoTutorial</div>
+  )
+}
+
+export default MemoTutorial
